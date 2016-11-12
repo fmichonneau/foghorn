4a14c8e944ca96754d19802651538b35861bb858
Francois Michonneau <francois.michonneau@gmail.com>
2016-11-12 14:46:21 -0500

    [fix #1, fix #2] add tests and test coverage

---
+++ tests/testthat.R
@@ -0,0 +1,5 @@
+library(testthat)
+library(foghorn)
+httrmock::start_replaying()
+test_check("foghorn")
+httrmock::stop_replaying()
---
+++ tests/testthat/test-foghorn.R
@@ -0,0 +1,53 @@
+context("fail for invalid package name/emails")
+
+test_that("invalid package name", {
+    expect_error(cran_check_results(package = "foobarfoobar"),
+                 "Invalid package name")
+})
+
+test_that("invalid email address", {
+    expect_error(cran_check_results(email = "foobar"),
+                 "Malformed email address")
+    expect_error(cran_check_results(email = "foo@foobarbar.rrrr"),
+                 "Invalid email address")
+})
+
+context("cran check results")
+
+check_cran_check_results <- function(x) {
+    length(x) == 6 &&
+        inherits(x, "tbl_df") &&
+        all(names(x) %in% c("Package", "ERROR", "WARN", "NOTE",  "OK", "has_memtest_notes")) &&
+        nrow(x) > 0
+}
+
+test_that("at least email or package name specified", {
+    expect_error(cran_check_results(), "provide at least one value for")
+})
+
+test_that("works with one package, one address, or both", {
+    res_pkg <- cran_check_results(package = "phylobase")
+    res_email <- cran_check_results(email = "francois.michonneau@gmail.com")
+    res_both <- cran_check_results(email = "francois.michonneau@gmail.com",
+                                   package = "ridigbio")
+    expect_true(check_cran_check_results(res_pkg))
+    expect_true(check_cran_check_results(res_email))
+    expect_true(check_cran_check_results(res_both))
+})
+
+test_that("works for maintainers with a single package", {
+    res <- cran_check_results(email = "bbolker+lme4@gmail.com")
+    expect_true(check_cran_check_results(res))
+})
+
+test_that("works for multiple packages, multiple addresses", {
+    res_pkgs <- cran_check_results(package = c("rotl", "phylobase", "ridigbio"))
+    res_emails <- cran_check_results(email = c("francois.michonneau@gmail.com",
+                                               "hadley@rstudio.com"))
+    res_both <- cran_check_results(email =   c("francois.michonneau@gmail.com",
+                                               "hadley@rstudio.com"),
+                                   package = c("ridigbio", "mregions", "bold"))
+    expect_true(check_cran_check_results(res_pkgs))
+    expect_true(check_cran_check_results(res_emails))
+    expect_true(check_cran_check_results(res_both))
+})
