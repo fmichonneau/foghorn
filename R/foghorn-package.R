40a001f414f9a6f7016bfd55a8780b46e26199a1
Francois Michonneau <francois.michonneau@gmail.com>
2016-11-17 14:40:00 -0500

    [fix #3] add help page for package

---
+++ R/foghorn-package.R
@@ -0,0 +1,11 @@
+##' CRAN check results at the terminal
+##'
+##' \code{foghorn} brings the results of the CRAN checks into your R
+##' terminal. The results of the checks can be summarized as a
+##' tibble, or as a summary.
+##'
+##' The package also provides functions to view the logs of the CRAN
+##' check results, or to visit the web pages hosted by CRAN.
+##' @name foghorn
+##' @docType package
+NULL
