42a757f0fa549e5a018f8d5335ba1595401dadb8
Francois Michonneau <francois.michonneau@gmail.com>
2016-11-19 20:05:33 -0500

    make email address lowercase

--- R/foghorn.R
+++ R/foghorn.R
@@ -9,7 +9,7 @@ url_email_res <- function(email) {
         if (!grepl("\\@", x)) {
             stop("Malformed email address: ", sQuote(email), call. = FALSE)
         })
-    email <- gsub("\\@", "_at_", email)
+    email <- gsub("\\@", "_at_", tolower(email))
     ##  "all characters different from letters, digits, hyphens,
     ##  underscores, colons, and periods replaced by underscores ..."
     email <- gsub("[^[:alnum:]_:.-]","_",email)
