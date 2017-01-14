82fb832927a52eef78328883b6ca993f1b90f0af
Francois Michonneau <francois.michonneau@gmail.com>
2017-01-13 21:23:57 -0800

    create table_cran_checks method for crandb_email class

--- R/cran_files.R
+++ R/cran_files.R
@@ -1,6 +1,8 @@
 fetch_cran_rds_file <- function(file = c("details", "results", "flavors"),
                                 dest = tempdir(), protocol = c("https", "ftp"),
                                 ...) {
+    ## TODO -- need to check for internet connection
+
     file <- match.arg(file)
     protocol <- match.arg(protocol)
     is_ftp <- if (identical(protocol, "ftp")) "pub/R/" else character(0)
@@ -19,6 +21,18 @@ read_cran_rds_file <- function(file) {
 }
 
 maintainer_pkg_info <- function(email, crandb) {
+    if (!exists("Maintainer", crandb))
+        stop("use `results` database")
     idx <- grepl(paste0("<", email, ">"), crandb[["Maintainer"]])
-    crandb[idx, ]
+    res <- crandb[idx, ]
+    class(res) <- c("crandb_email", class(res))
+    res
+}
+
+table_cran_checks.crandb_email <- function(parsed, ...) {
+    parsed %>%
+        dplyr::count_(vars = c("Package", "Status")) %>%
+        tidyr::spread_("Status", "n") %>%
+        dplyr::bind_rows(default_cran_checks) %>%
+        dplyr::ungroup()
 }
