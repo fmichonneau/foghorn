51310754c4a203af4bd595235d6d6ad7c4ce84c4
Francois Michonneau <francois.michonneau@gmail.com>
2017-01-13 21:23:39 -0800

    make variable for destination file

--- R/cran_files.R
+++ R/cran_files.R
@@ -5,9 +5,10 @@ fetch_cran_rds_file <- function(file = c("details", "results", "flavors"),
     protocol <- match.arg(protocol)
     is_ftp <- if (identical(protocol, "ftp")) "pub/R/" else character(0)
     file <- paste0("check_", file, ".rds")
+    dest_files <- file.path(dest, file)
     cran_url <- paste0(protocol, "://cran.r-project.org/", is_ftp, "web/checks/", file)
-    download.file(url = cran_url, destfile = file.path(dest, file), ...)
-    invisible(file.path(dest, file))
+    download.file(url = cran_url, destfile = dest_files, ...)
+    invisible(dest_files)
 }
 
 
