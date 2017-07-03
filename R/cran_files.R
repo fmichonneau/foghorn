02b3ea80a1df6f05f04ff3efe8c1b5efe58eaeab
Francois Michonneau <francois.michonneau@gmail.com>
2017-07-03 18:08:51 -0400

    better handling of crandb files

--- R/cran_files.R
+++ R/cran_files.R
@@ -1,12 +1,12 @@
 ##' @importFrom httr GET write_disk status_code
 fetch_cran_rds_file <- function(file = c("details", "results", "flavors", "issues"),
                                 dest = tempdir(), protocol = c("https", "http"),
-                                overwrite = FALSE, ...) {
+                                overwrite = FALSE, file_prefix = NULL, ...) {
     file <- match.arg(file)
     protocol <- match.arg(protocol)
     is_ftp <- if (identical(protocol, "ftp")) "pub/R/" else character(0)
     file <- paste0("check_", file, ".rds")
-    dest_file <- file.path(dest, file)
+    dest_file <- file.path(dest, paste0(file_prefix, file))
     if (! (file.exists(dest_file) &&
            file.size(dest_file) > 0) ||
         overwrite) {
@@ -19,23 +19,40 @@ fetch_cran_rds_file <- function(file = c("details", "results", "flavors", "issue
     invisible(dest_file)
 }
 
-
-read_cran_rds_file <- function(file) {
+check_cran_rds_file <- function(file, return_logical = FALSE) {
     if (!file.exists(file))
         stop(file, " can't be found...", call. = FALSE)
     if (file.size(file) < 100)
-        stop(file, " is corrupted. Delete it and retry")
-    res <- readRDS(file = file)
+        stop(file, " is corrupted. Delete it and retry.")
+    res <- try(readRDS(file = file), silent = TRUE)
+    if (inherits(res, "try-error"))
+        stop(file, " can't be read. Delete it and retry.")
+    else if (return_logical)
+        return(TRUE)
+    else res
+}
+
+read_cran_rds_file <- function(file) {
+    res <- check_cran_rds_file(file)
     class(res) <- c("cran_db", class(res))
     res
 }
+
+## file can be a valid RDS file downloaded manually (or using a custom
+## fetch_cran_rds_file() call), or the type of file that one wants to
+## download from CRAN servers (e.g., "results", "details", "issues", ...)
 get_cran_rds_file <- function(file, ...) {
-    f <- fetch_cran_rds_file(file, ...)
+    if (grepl("\\.rds$", file, ignore.case = TRUE) &&
+        file.exists(file)) {
+        f <- file
+    } else {
+        f <- fetch_cran_rds_file(file, ...)
+    }
     read_cran_rds_file(f)
 }
 
-crandb_pkg_info_email <- function(email, ...) {
-    crandb <- get_cran_rds_file(file = "results", ...)
+crandb_pkg_info_email <- function(email, file = "results", ...) {
+    crandb <- get_cran_rds_file(file = file, ...)
     maintainer <- tolower(crandb$Maintainer)
     idx <- lapply(tolower(email), function(x)
         grepl(paste0("<", x, ">"), maintainer, fixed = TRUE))
@@ -46,8 +63,8 @@ crandb_pkg_info_email <- function(email, ...) {
     res
 }
 
-crandb_pkg_info_pkg <- function(pkg, ...) {
-    crandb <- get_cran_rds_file(file = "results", ...)
+crandb_pkg_info_pkg <- function(pkg, file = "results", ...) {
+    crandb <- get_cran_rds_file(file = file, ...)
     res <- crandb[crandb[["Package"]] %in% pkg, ]
     class(res) <- c("crandb", class(res))
     res
