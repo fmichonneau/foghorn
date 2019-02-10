6ec70329b4cada1b646f247e9be5fd43ffabce82
François Michonneau <francois.michonneau@gmail.com>
2019-02-10 08:40:05 -0500

    consolidate redo

--- tests/testthat/test-n_cran_platforms.R
+++ tests/testthat/test-n_cran_platforms.R
@@ -1,8 +1,22 @@
 context("check that the number of CRAN platforms match the global variable")
 
-test_that("n_cran_platforms matches number listed on their website", {
-  skip_on_cran()
-  cran_platforms_page <- xml2::read_html("https://cran.r-project.org/web/checks/check_flavors.html")
-  cran_platforms_table <- rvest::html_table(cran_platforms_page)[[1]]
-  expect_true(nrow(cran_platforms_table) == n_cran_platforms)
+validate_n_flavors <- function() {
+    cran_flavors <- try(
+        xml2::read_html("https://cran.rstudio.org/web/checks/check_flavors.html"),
+        silent = TRUE)
+    if (inherits(cran_flavors, "try-error")) {
+        warning("Can't connect to check number of flavors. Using hardcoded value.")
+        return(12L)
+    }
+
+    cran_flvr_tbl <- cran_flavors %>%
+        rvest::html_node("table") %>%
+        rvest::html_table()
+
+    nrow(cran_flvr_tbl)
+}
+
+test_that("foghorn uses the accurate number of CRAN flavors", {
+    skip_on_cran()
+    expect_true(identical(validate_n_flavors(), 12L))
 })
